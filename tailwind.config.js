03385a9a42f12207108450bd25733ca97d00a9a5
Akpan Abraham <Akpjunio4real@gmail.com>
2020-06-18 13:55:18 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily:{
+      'sans': ['Nunito', 'system-ui','BlinkMacSystemFont','-apple-system', ...],
+      'serif': ['Roboto','Oxygen','Ubuntu', ...],
+      'mono': ['Helvetica' 'Neue','sans-serif', ...],
+    },
+    
+    extend: {
+      colors: {
+        darkerblue: '#021764'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
